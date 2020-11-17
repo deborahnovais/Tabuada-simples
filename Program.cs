84058247b9c4f1ed3e21e81d92165e9e4bf8445d
Deborah Novais <61918962+deborahnovais@users.noreply.github.com>
2020-11-17 18:38:53 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            Console.WriteLine("Digite o número que você quer ver a tabuada.");
+            numero = int.Parse(Console.ReadLine());
+
+            for(int n = 1; n <= 10; n++) {
+
+                resultado = numero * n;
+                Console.WriteLine(n+ " X " +numero+ " = " +resultado);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
